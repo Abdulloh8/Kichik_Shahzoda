462994d5da34d70dbbb4fc547170ec17baac9997
Your Name <you@example.com>
2024-04-25 15:38:45 +0500

    Abdulloh8/proyektTugatildi

---
+++ Aylana.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Kichik_Shahzoda
+{
+    public struct Aylana
+    {
+        public Nuqta Markaz { get; set; }
+        public int Radius { get; set; }
+        public Aylana() : this(new Nuqta(), 0) { }
+        public Aylana(Nuqta markaz, int radius) => (Markaz, Radius) = (markaz, radius);
+        public readonly bool IsIn(Nuqta nuqta) => Markaz.DistanceTo(nuqta) < Radius;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Kichik_Shahzoda;
+
+
+
+var  testCases = int.Parse(Console.ReadLine() ?? "0");
+
+while (testCases-- > 0)
+{
+    var kesishlarSoni = 0;
+    var kordinatalar = Console.ReadLine()?
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    var shaxzoda = new Nuqta(kordinatalar![0], kordinatalar![1]);
+    var malika = new Nuqta(kordinatalar![2], kordinatalar![3]);
+
+    var planetalarSoni = int.Parse(Console.ReadLine() ?? "0");
+
+    while (planetalarSoni-- > 0)
+    {
+        
+        var planeteDate = Console.ReadLine()?
+           .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+           .Select(int.Parse)
+           .ToArray();
+
+        var markaz = new Nuqta(planeteDate![0], planeteDate![1]);
+        var planeta = new Aylana(markaz , planeteDate![2]);
+
+        var shahzodaIchida = planeta.IsIn(shaxzoda);
+        var malikaIchida = planeta.IsIn(malika);
+
+        var kesibOtadimi = shahzodaIchida ^ malikaIchida;
+        if (kesibOtadimi)
+            kesishlarSoni++;
+    }
+    Console.WriteLine(kesishlarSoni);
+}
+
